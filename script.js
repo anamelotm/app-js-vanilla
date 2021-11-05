1796cf3ae9d6a6552a2a4f105f2ddc4072b48421
AnaMelo <analuisa.tomita@hotmail.com>
2021-11-05 12:27:11 -0300

    implementado metodos de somar, mostar saldos e excluir

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const transactionsUl = document.querySelector('#transactions');
 const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount = document.querySelector('#amount');
+const balanceDisplay = document.querySelector('#balance');
+const incomeDisplay = document.querySelector('#money-plus');
+const expenseDisplay = document.querySelector('#money-minus');
 
 //objeto literal ficticio
 let dummyTransactions = [
@@ -10,9 +13,12 @@ let dummyTransactions = [
     {id: 4, name: 'Violão', amount: 150}
 ]
 
+//gerador de id numero aleatorio
+const geradorID = () => Math.round(Math.random() * 1000);
+
 const addTransactionArray = (transactionName, transactionAmount) => {
     dummyTransactions.push({
-        id: 123, 
+        id: geradorID(), 
         name: transactionName.value, 
         amount: transactionAmount.value});
 }
@@ -34,10 +40,19 @@ form.addEventListener("submit", handleFormSubmit);
 
 const addTransactionsIntoDOM = (transaction) => {
     const li = document.createElement('li');
-    li.innerHTML = `${transaction.name}`;
+    
+    li.innerHTML = `
+        ${transaction.name}
+        <span> ${transaction.amount} </span>
+        <button onClick="removeTransaction(${transaction.id})">X</button>
+    `;
     //atribuindo um nó para o li
     transactionsUl.appendChild(li);
+
+    console.log(dummyTransactions);
 }
+
+
 
 //metodo que ira separar os valores de total, receitas e despesas
 const updateBalanceValues = () => {
@@ -49,19 +64,33 @@ const updateBalanceValues = () => {
     //totalizador
     const total = transactionAmounts.reduce((accumulator, transaction) => accumulator + transaction,0);
     console.log('Soma dos valores: ' + total);
+    total.toLocaleString('pt-br', {style:'currency', currency:'BRL'});
 
     const income = transactionAmounts.filter(value => value > 0).reduce((accumulator, transaction) => accumulator + transaction, 0);
     console.log('Somente os valores positivos ' + income);
 
     const expense = transactionAmounts.filter(value => value < 0).reduce((accumulator, transaction) => accumulator + transaction,0);
     console.log('Some os valores negativos: '+ expense);
+
+    balanceDisplay.textContent = total.toLocaleString('pt-br', {style:'currency', currency:'BRL'});
+    incomeDisplay.textContent = income.toLocaleString('pt-br', {style:'currency', currency:'BRL'});
+    expenseDisplay.textContent = expense.toLocaleString('pt-br', {style:'currency', currency:'BRL'});
+ 
 }
 
-updateBalanceValues();
+const removeTransaction = ID => {
+    dummyTransactions = dummyTransactions.filter(transactions => transactions.id !== ID);
+    console.log(dummyTransactions);
+
+    init();
+}
+
+
 const init = () => {
     //aqui iremos fazer um tratamento a nivel de codigo para nao submeter toda a lista novamente
     transactionsUl.innerHTML = '';
-    dummyTransactions.forEach(addTransactionsIntoDOM)
+    dummyTransactions.forEach(addTransactionsIntoDOM);
+    updateBalanceValues();  
 }
 
 // funcao de inicializacao do js
