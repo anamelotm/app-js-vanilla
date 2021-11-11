22579eaa3598a376bff621a95c2b3daa52b86bc5
AnaMelo <analuisa.tomita@hotmail.com>
2021-11-11 14:49:53 -0300

    excluida pasta api-controle

--- api-controle-despesa/src/main/java/com/controle_despesa/model/repositories/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.controle_despesa.model.repositories;
-
-import com.controle_despesa.model.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
